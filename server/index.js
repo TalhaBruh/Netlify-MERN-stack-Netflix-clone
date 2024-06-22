998a9a19372e6dbcae4f19dd544d827425bb0e66
Talha Hussain <talhahussain349@gmail.com>
2024-06-22 21:21:24 +0500

    formatted docement with prettier

--- server/index.js
+++ server/index.js
@@ -19,14 +19,17 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-  server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected");
+    server.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
   });
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
 
 //test
