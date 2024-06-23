2ea55540b3d6cf38ad40932c62c67f0d27eff1fd
Talha Hussain <talhahussain349@gmail.com>
2024-06-24 00:09:14 +0500

    ..

--- server/src/routes/review.route.js
+++ server/src/routes/review.route.js
@@ -6,36 +6,32 @@ import requestHandler from "../handlers/request.handler.js";
 
 const router = express.Router({ mergeParams: true });
 
-router.get(
-  "/",
-  tokenMiddleware.auth,
-  reviewController.getReviewsOfUser
-);
+router.get("/", tokenMiddleware.auth, reviewController.getReviewsOfUser);
 
 router.post(
   "/",
   tokenMiddleware.auth,
   body("mediaId")
-    .exists().withMessage("mediaId is required")
-    .isLength({ min: 1 }).withMessage("mediaId can not be empty"),
+    .exists()
+    .withMessage("mediaId is required")
+    .isLength({ min: 1 })
+    .withMessage("mediaId can not be empty"),
   body("content")
-    .exists().withMessage("content is required")
-    .isLength({ min: 1 }).withMessage("content can not be empty"),
+    .exists()
+    .withMessage("content is required")
+    .isLength({ min: 1 })
+    .withMessage("content can not be empty"),
   body("mediaType")
-    .exists().withMessage("mediaType is required")
-    .custom(type => ["movie", "tv"].includes(type)).withMessage("mediaType invalid"),
-  body("mediaTitle")
-    .exists().withMessage("mediaTitle is required"),
-  body("mediaPoster")
-    .exists().withMessage("mediaPoster is required"),
+    .exists()
+    .withMessage("mediaType is required")
+    .custom((type) => ["movie", "tv"].includes(type))
+    .withMessage("mediaType invalid"),
+  body("mediaTitle").exists().withMessage("mediaTitle is required"),
+  body("mediaPoster").exists().withMessage("mediaPoster is required"),
   requestHandler.validate,
   reviewController.create
 );
 
-router.delete(
-  "/:reviewId",
-  tokenMiddleware.auth,
-  reviewController.remove
-);
+router.delete("/:reviewId", tokenMiddleware.auth, reviewController.remove);
 
 export default router;
