3644709301d6d7fd224a512f6838bb6936f45ed2
Talha Hussain <talhahussain349@gmail.com>
2024-06-23 21:39:40 +0500

    .

--- server/src/models/favorite.model.js
+++ server/src/models/favorite.model.js
@@ -3,32 +3,35 @@ import modelOptions from "./model.options.js";
 
 export default mongoose.model(
   "Favorite",
-  mongoose.Schema({
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true
+  mongoose.Schema(
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      mediaType: {
+        type: String,
+        enum: ["tv", "movie"],
+        required: true,
+      },
+      mediaId: {
+        type: String,
+        required: true,
+      },
+      mediaTitle: {
+        type: String,
+        required: true,
+      },
+      mediaPoster: {
+        type: String,
+        required: true,
+      },
+      mediaRate: {
+        type: Number,
+        required: true,
+      },
     },
-    mediaType: {
-      type: String,
-      enum: ["tv", "movie"],
-      required: true
-    },
-    mediaId: {
-      type: String,
-      required: true
-    },
-    mediaTitle: {
-      type: String,
-      required: true
-    },
-    mediaPoster: {
-      type: String,
-      required: true
-    },
-    mediaRate: {
-      type: Number,
-      required: true
-    },
-  }, modelOptions)
+    modelOptions
+  )
 );
