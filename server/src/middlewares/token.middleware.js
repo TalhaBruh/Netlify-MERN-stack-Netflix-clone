11571a9a1d818ea24d2f54d6cad68118b2a4753b
Talha Hussain <talhahussain349@gmail.com>
2024-06-23 21:39:24 +0500

    token middleware

--- server/src/middlewares/token.middleware.js
+++ server/src/middlewares/token.middleware.js
@@ -9,10 +9,7 @@ const tokenDecode = (req) => {
     if (bearerHeader) {
       const token = bearerHeader.split(" ")[1];
 
-      return jsonwebtoken.verify(
-        token,
-        process.env.TOKEN_SECRET
-      );
+      return jsonwebtoken.verify(token, process.env.TOKEN_SECRET);
     }
 
     return false;
