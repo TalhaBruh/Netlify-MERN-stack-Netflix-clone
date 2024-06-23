b977cacb946df2935bbe0b250fcde236a34a9b12
Talha Hussain <talhahussain349@gmail.com>
2024-06-23 21:38:46 +0500

    .

--- server/src/handlers/response.handler.js
+++ server/src/handlers/response.handler.js
@@ -1,28 +1,33 @@
-const responseWithData = (res, statusCode, data) => res.status(statusCode).json(data);
+const responseWithData = (res, statusCode, data) =>
+  res.status(statusCode).json(data);
 
-const error = (res) => responseWithData(res, 500, {
-  status: 500,
-  message: "Oops! Something worng!"
-});
+const error = (res) =>
+  responseWithData(res, 500, {
+    status: 500,
+    message: "Oops! Something worng!",
+  });
 
-const badrequest = (res, message) => responseWithData(res, 400, {
-  status: 400,
-  message
-});
+const badrequest = (res, message) =>
+  responseWithData(res, 400, {
+    status: 400,
+    message,
+  });
 
 const ok = (res, data) => responseWithData(res, 200, data);
 
 const created = (res, data) => responseWithData(res, 201, data);
 
-const unauthorize = (res) => responseWithData(res, 401, {
-  status: 401,
-  message: "Unathorized"
-});
+const unauthorize = (res) =>
+  responseWithData(res, 401, {
+    status: 401,
+    message: "Unathorized",
+  });
 
-const notfound = (res) => responseWithData(res, 404, {
-  status: 404,
-  message: "Resource not found"
-});
+const notfound = (res) =>
+  responseWithData(res, 404, {
+    status: 404,
+    message: "Resource not found",
+  });
 
 export default {
   error,
@@ -30,5 +35,5 @@ export default {
   ok,
   created,
   unauthorize,
-  notfound
+  notfound,
 };
