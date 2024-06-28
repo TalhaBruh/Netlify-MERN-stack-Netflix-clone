e8b64512a184f691cb04ae0813207905dbdf72b7
Talha Hussain <talhahussain349@gmail.com>
2024-06-28 07:21:36 +0500

    .

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Typography, useTheme } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   const theme = useTheme();
 
   return (
     <Typography variant="h6" fontWeight="bold" fontSize="1.7rem">
-      <Link to="/" style={{ textDecoration: 'none' }}>
+      <Link to="/" style={{ textDecoration: "none" }}>
         <span style={{ color: theme.palette.primary.main }}>Netlify</span>
       </Link>
     </Typography>
