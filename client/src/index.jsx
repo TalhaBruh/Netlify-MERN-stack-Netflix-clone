1ead1253e39c8707cdffaa9cf4dcc6e460c400de
Talha Hussain <talhahussain349@gmail.com>
2024-07-01 23:38:29 +0500

    .

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.St
+  // <React.S
   <Provider store={store}>
     <App />
   </Provider>
