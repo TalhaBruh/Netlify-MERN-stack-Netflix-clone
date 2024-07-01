95e239af9ba42ea59a114fc2328f5e1a978347e1
Talha Hussain <talhahussain349@gmail.com>
2024-07-01 23:35:56 +0500

    .

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
+  // <React.StrictMode
   <Provider store={store}>
     <App />
   </Provider>
